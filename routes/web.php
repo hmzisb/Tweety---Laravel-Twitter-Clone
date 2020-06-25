63cd98efcce727b5a5e25ee4367a96fea3691a81
Hamza Ahmed <hmzisb@Hamzas-MacBook-Pro.local>
2020-06-25 07:46:01 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-// DB::listen(function($query){ var_dump($query->sql, $query->bindings); });
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,13 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
 Route::middleware('auth')->group(function(){
+	Route::get('/', 'TweetController@index');
 	Route::get('/tweets', 'TweetController@index')->name('home');
 	Route::post('/tweets', 'TweetController@store')->name('Post-Tweet');
 	Route::post('/profiles/{user:username}/follow', 'FollowsController@store');
